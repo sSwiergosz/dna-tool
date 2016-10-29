51dfeaa527e669981741b2e6992504d4e55b9bb2
sSwiergosz <szymon.swiergosz@gmail.com>
2016-10-29 15:26:36 +0200

    Final version

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $("button").on("click", function(z) {
+        z.preventDefault();
+        var e = document.getElementById("optionList");
+        var selected = e.options[e.selectedIndex].value;
+        var inputValue = $("#seq").val();
+        var str = '';
+        var compBases = {
+            A: "T",
+            T: "A",
+            G: "C",
+            C: "G"
+        };
+
+        for (var i in inputValue) {
+        	if (!(inputValue[i] in compBases)) {
+        		return  $("#result").html("Error! In DNA there are only A,G,C or T.");
+        	}
+        }
+
+        if (selected == 'reverse') {
+            $("#result").html(inputValue.split('').reverse().join(''));
+        } 
+
+        else if (selected == 'complementary') {
+            for (var i in inputValue) {
+                str += inputValue[i].replace(/[ATGC]/, function(c) {
+                    return compBases[c];
+                });
+            }
+            $("#result").html(str);
+
+        } 
+
+        else if (selected = 'reverse complementary') {
+            for (var i in inputValue) {
+                str += inputValue[i].replace(/[ATGC]/, function(c) {
+                    return compBases[c];
+                });
+            }
+            $("#result").html(str.split('').reverse().join(''));
+        }
+    });
+});
